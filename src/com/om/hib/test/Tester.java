adbc6bdbeae57ed9c1d56e40bde9e1064fae98a8
shakuntala <shakuntala@dell>
2015-05-23 16:16:00 +0530

    check in is done to HibernateCreate

---
+++ src/com/om/hib/test/Tester.java
@@ -0,0 +1,33 @@
+package com.om.hib.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.om.hib.dto.PersonDTO;
+
+public class Tester {
+	public static void main(String[] args){
+		PersonDTO personDTO=new PersonDTO();
+		personDTO.setName("om");
+		personDTO.setPlace("bangalore");
+		Configuration configuration=new Configuration();
+		configuration.configure();
+		SessionFactory sessionFactory=configuration.buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		Transaction transaction=session.beginTransaction();
+		try{
+			transaction.begin();
+			session.save(personDTO);
+			transaction.commit();
+		}catch(HibernateException exception){
+			transaction.rollback();
+			exception.printStackTrace();
+		}finally{
+			session.close();
+		}
+	}
+
+}
